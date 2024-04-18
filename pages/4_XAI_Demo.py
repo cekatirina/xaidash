f8259f307fc221977325cbe01a3b0639af1fdf83
cekatirina <135237985+cekatirina@users.noreply.github.com>
2024-04-18 17:25:07 +0000

    xai dash text

--- pages/4_XAI_Demo.py
+++ pages/4_XAI_Demo.py
@@ -5,3 +5,9 @@ import pandas as pd
 
 import streamlit as st
 from streamlit.hello.utils import show_code
+
+st.set_page_config(page_title="XAI Dash", page_icon="💖")
+st.markdown("# XAI Dash")
+st.write(
+    """This is a page where XAI dashboard will be."""
+)
